48067c3ed9798df39fbd41c7d6366a745ff378c5
tiwarirahul28 <tiwarirahul28042002@gmail.com>
2024-03-26 16:57:26 +0530

    first commit

---
+++ Weather/scripts/scripts.js
@@ -0,0 +1,53 @@
+const apikey = "13ffbfc5d389f11b210cc1e23058d10e";
+const apiurl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox = document.getElementById("cityInput");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function getWeather(city) {
+  try {
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    if (response.status !== 404) {
+      // Corrected the condition
+      const data = await response.json();
+      console.log(data);
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML =
+        Math.round(data.main.temp) + "°C";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+      if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "/Weather/images/clouds.png";
+      } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "/Weather/images/clear.png";
+      } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "/Weather/images/rain.png";
+      } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "/Weather/images/drizzle.png";
+      } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "/Weather/images/mist.png";
+      }
+      document.querySelector(".error").style.display = "none";
+      document.querySelector(".weather").style.display = "block";
+    } else {
+      document.querySelector(".error").style.display = "block";
+      document.querySelector(".weather").style.display = "none";
+    }
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+}
+
+searchbtn.addEventListener("click", () => {
+  const city = searchbox.value.trim();
+  if (city !== "") {
+    getWeather(city);
+  } else {
+    alert("Please enter a city name.");
+  }
+});
+
+// Initial call to fetch Mumbai weather
+// getWeather("Mumbai");
+getWeather();
