c1ec58da46f592765153ff0fc293c2e7b3d99ef5
tiwarirahul28 <tiwarirahul28042002@gmail.com>
2024-04-03 15:17:33 +0530

    Quiz App

---
+++ Scripts.js
@@ -0,0 +1,53 @@
+// let lengt = "Lhcdbshyew /t Ehduo;sijlk/";
+// console.log("lengt\nwjeadwkxz");
+// console.log("lengt\twjeadwkxz");
+
+// let Fullname = prompt("Enter your Full Name");
+// let username = "@" + Fullname + Fullname.length;
+// console.log(username);
+
+// const colors = ["red", "yellow", "blue"];
+// forlooop
+// for (let i = 0; i < colors.length; i++){
+//     console.log(colors[i].toUpperCase());
+// }
+// for of
+// for(let color of colors){
+//     console.log(color);
+// }
+
+// let marks = [33, 44, 55, 66, 77];
+// let sum = 0;
+// for (let val of marks) {
+//   console.log(val);
+//   sum += val;
+// }
+// let avg = sum / marks.length;
+// console.log(`The average mark is ${avg}`);
+
+// let item = [250, 645, 300, 900, 50];
+// let i = 0;
+// for (let val of item) {
+//   item * prices;
+//   console.log(`value at index ${i} = ${val}`);
+//   let offer = val / 10;
+//   item[i] = item[i] - offer;
+//   console.log(`vqlue after offer = ${item[i]}`);
+//   i++;
+// }
+
+// for (let i = 0; i < item.length; i++) {
+//   let offer = item[1] / 10;
+//   item[i] -= offer;
+// }
+// console.log(`vqlue after offer = ${item}`);
+
+// let num = [11, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+// num.splice(1, 0, 55, 66, 66);
+// console.log(num);
+
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Amazon"];
+// companies.shift();
+// companies.splice(2, 1, "Ola")
+companies.push("Netflix");
+console.log(companies);
